c2c6f3b64595781244c1e48f61d520d64c89785c
AKgalv1x <akgalvis@gmail.com>
2025-05-07 08:15:11 -0500

    Pilas_Sharick

---
+++ sharick.php
@@ -0,0 +1,18 @@
+<?php
+  $ropa = array();
+
+  array_push($ropa,"chaqueta");
+  array_push($ropa,"buzo");
+  array_push($ropa,"falda");
+  array_push($ropa,"sueter");
+  array_push($ropa,"pantalón");
+  array_push($ropa,"bufanda");
+  array_push($ropa,"gorro");
+  array_push($ropa,"zapatos");
+  array_push($ropa,"camisa");
+
+  $closet = array_pop($ropa); 
+
+  echo "la última prenda a mostrarse es " .$closet;
+
+  ?>
